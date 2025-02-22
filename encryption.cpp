bbe49724e1a4c211c220d497f5813a961d0f8d09
Eric Solorzano <esolorzano8@csu.fullerton.edu>
2025-02-21 16:01:02 -0800

    Fixed some syntax errors and typos. Also discovered that I cannot have a dynamic array so I updated it to be a two dimensional vector for the matrices.

--- encryption.cpp
+++ encryption.cpp
@@ -1,3 +1,3 @@
-#include "ecryption.hpp"
+#include "encryption.hpp"
 
 
--- encryption.hpp
+++ encryption.hpp
@@ -6,33 +6,28 @@
 #include <iostream>
 #include <vector>
 class encryption {
-
-
 public:
-std::string getMessage() {return message;}
-std::string getEncryptedMessage() {return encrypted_message;}
-int getMessageSize() {return message_size;}
-int getN() {return n;}
-void parse();
-void insertInMatrix();
-void transposeMatrix();
-void sendToFile();
-void shiftOperations();
-void unParse();
-
+	encryption(const std::string& msg);
+	std::string getMessage() { return message; }
+	std::string getEncryptedMessage() { return encrypted_message; }
+	int getMessageSize() { return message_size; }
+	int getN() { return n; }
+	void parse();
+	void insertInMatrix();
+	void transposeMatrix();
+	void sendToFile();
+	void shiftOperations();
+	void unParse();
 
 private:
-std::string message;
-std::string encrypted_message;
-int message_size;
-int n; // n represents the number of rows and columns of the matrix derived by taking the square root of message_size
-std::vector<char> OGparsed;
-std::vector<char> ENparsed;
-char matrix[n][n];
-char matrix_transposed[n][n]
-
-
-
-}
+	std::string message;
+	std::string encrypted_message;
+	int message_size;
+	int n; // n represents the number of rows and columns of the matrix derived by taking the square root of message_size
+	std::vector<char> OGparsed;
+	std::vector<char> ENparsed;
+	std::vector<std::vector<char>> matrix;
+	std::vector<std::vector<char>> matrixTransposed;
+};
 
 #endif
