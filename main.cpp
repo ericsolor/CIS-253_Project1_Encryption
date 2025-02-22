7b6fdeae4752540758c3e6310a9033abd9e2154e
Eric Solorzano <esolorzano8@csu.fullerton.edu>
2025-02-21 19:59:50 -0800

    Began writing the functionality of main.cpp, just a start no where near complete, moving onto the decryption class

--- main.cpp
+++ main.cpp
@@ -1,1 +1,32 @@
 #include <iostream>
+#include "encryption.hpp"
+#include "decryption.hpp"
+
+int main() {
+	int userChoice = 0;
+	std::string userInput;
+	std::string msg, line;
+	std::ifstream file;
+	std::cout << "Eric Solorzano's Encryption Project \nPlease select a menu option below: \n";
+	std::cin >> userChoice;
+
+	switch (userChoice) {
+	case 1: // (Encrypt message)
+		std::cout << "Please enter path to the message .txt file: ";
+		std::getline(std::cin, userInput);
+		file.open(userInput);
+
+		while (std::getline(file, line)) {
+			msg += line;
+		}
+		file.close();
+		break;
+	case 2: // (Decrypt Message)
+		break;
+	case 3: // (Test)
+		break;
+	case 4: // (Exit)
+		break;
+	}
+	return 0;
+}
